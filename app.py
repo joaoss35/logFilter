2b9764b033a09f15769e542829d1328a2b367f4e
João Pacheco <joao.pacheco@kununu.com>
2023-05-31 19:36:44 +0100

    Add LOG_LEVEL support

--- app.py
+++ app.py
@@ -16,12 +16,33 @@ def find(pattern, path):
     return result
 
 
+def get_markers_level(log_level) -> list:
+    match log_level:
+        case 0:
+            markers = [constants.CONSTANTS.MARKERS[0]]
+        case 1:
+            markers = constants.CONSTANTS.MARKERS[:2]
+        case 2:
+            markers = constants.CONSTANTS.MARKERS
+        case _:
+            markers = []
+    return markers
+
+
+
 def app():
     log_file_names = find(constants.CONSTANTS.LOGS_PATTERN, constants.CONSTANTS.LOGS_DIRECTORY)
     last_printed_line = 0
 
+    log_level = int(os.environ.get('LOG_LEVEL', '0'))
+    markers = get_markers_level(log_level)
+
     if not log_file_names:
         print(constants.CONSTANTS.NO_LOGS_FOUND_TEXT)
+        sys.exit(0)
+
+    if not markers:
+        print(constants.CONSTANTS.NO_MARKERS_FOUND_TEXT)
         sys.exit(0)
 
     # Keep listening on the log files for any incoming logs.
@@ -33,7 +54,7 @@ def app():
                 # Print only new unprinted lines
                 for i in range(last_printed_line, len(file_lines)):
                     line = file_lines[i]
-                    if any(marker in line for marker in constants.CONSTANTS.MARKERS):
+                    if any(marker in line for marker in markers):
                         sys.stdout.write(line)
                         last_printed_line = i + 1
         except (FileNotFoundError, PermissionError, OSError) as e:
--- constants/constants.py
+++ constants/constants.py
@@ -2,5 +2,7 @@ class CONSTANTS(enumerate):
     LOGS_DIRECTORY = "/var/log/app"
     LOGS_PATTERN = "*.log"
     NO_LOGS_FOUND_TEXT = "No log files found. Exiting..."
-    MARKERS = ["INFO", "WARNING", "ERROR"]
+    NO_MARKERS_FOUND_TEXT = "Please define a valid LOG_LEVEL value. Exiting..."
+    MARKERS = ["ERROR", "WARNING", "INFO"]
     ERROR_OPENING_LOG_FILE_TEXT = "Error opening log file"
+
