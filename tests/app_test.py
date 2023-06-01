f13f3ebc53afd14563f73d26ff2563746315fea9
João Pacheco <joao.pacheco@kununu.com>
2023-06-01 20:47:30 +0100

    Removed unused log files path from tests

--- tests/app_test.py
+++ tests/app_test.py
@@ -24,7 +24,6 @@ class LogFilterEngineTest(unittest.TestCase):
             "2023-05-30 15:56:40,303 ERROR: Failed to connect to the database"
         ]
         engine = app.LogFilterEngine()
-        engine.log_file_names = ["/var/log/app/test.log"]
         engine.update_log_level(1)
         filtered_logs = engine.filter_logs(logs)
         self.assertEqual(
@@ -42,7 +41,6 @@ class LogFilterEngineTest(unittest.TestCase):
             "2023-05-30 15:56:40,303 ERROR: Failed to connect to the database"
         ]
         engine = app.LogFilterEngine()
-        engine.log_file_names = ["/var/log/app/test.log"]
         engine.update_log_level(0)
         filtered_logs = engine.filter_logs(logs)
         self.assertEqual(
@@ -59,7 +57,6 @@ class LogFilterEngineTest(unittest.TestCase):
             "2023-05-30 15:56:40,303 ERROR: Failed to connect to the database"
         ]
         engine = app.LogFilterEngine()
-        engine.log_file_names = ["/var/log/app/test.log"]
         engine.last_printed_line = 3
         engine.update_log_level(2)
         filtered_logs = engine.filter_logs(logs)
@@ -75,7 +72,6 @@ class LogFilterEngineTest(unittest.TestCase):
             "2023-05-30 15:56:40,303 ERROR: Failed to connect to the database"
         ]
         engine = app.LogFilterEngine()
-        engine.log_file_names = ["/var/log/app/test.log"]
         engine.last_printed_line = 2
         engine.update_log_level(2)
         filtered_logs = engine.filter_logs(logs)
@@ -86,28 +82,24 @@ class LogFilterEngineTest(unittest.TestCase):
 
     def test_when_log_level_zero_is_updated_to_one_should_update_log_level_to_one(self):
         engine = app.LogFilterEngine()
-        engine.log_file_names = ["/var/log/app/test.log"]
         engine.log_level = 0
         engine.update_log_level(1)
         self.assertEqual(engine.log_level, 1)
 
     def test_when_log_level_one_is_updated_to_three_log_level_should_be_two(self):
         engine = app.LogFilterEngine()
-        engine.log_file_names = ["/var/log/app/test.log"]
         engine.log_level = 1
         engine.update_log_level(2)
         self.assertEqual(engine.log_level, 2)
 
     def test_when_markers_level_zero_is_updated_to_one_should_update_markers_level_to_one(self):
         engine = app.LogFilterEngine()
-        engine.log_file_names = ["/var/log/app/test.log"]
         engine.markers = []
         engine.update_log_level(1)
         self.assertEqual(engine.log_level, 1)
 
     def test_when_log_level_is_zero_should_return_updated_error_marker(self):
         engine = app.LogFilterEngine()
-        engine.log_file_names = ["/var/log/app/test.log"]
         engine.markers = []
         engine.log_level = 0
         engine.update_markers_level()
@@ -118,7 +110,6 @@ class LogFilterEngineTest(unittest.TestCase):
 
     def test_when_log_level_is_one_should_return_updated_error_and_warning_marker(self):
         engine = app.LogFilterEngine()
-        engine.log_file_names = ["/var/log/app/test.log"]
         engine.markers = []
         engine.log_level = 1
         engine.update_markers_level()
@@ -129,7 +120,6 @@ class LogFilterEngineTest(unittest.TestCase):
 
     def test_when_log_level_is_two_should_return_all_markers(self):
         engine = app.LogFilterEngine()
-        engine.log_file_names = ["/var/log/app/test.log"]
         engine.markers = []
         engine.log_level = 2
         engine.update_markers_level()
