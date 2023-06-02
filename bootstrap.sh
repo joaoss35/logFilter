099cb3631618089c24f519106ce3978c6b2b573a
João Pacheco <joao.pacheco@kununu.com>
2023-06-02 11:10:14 +0100

    Refactor bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,48 +1,52 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-function check_binaries()
-{
-  if ! command -v minikube &>/dev/null; then
-    echo "minikube is not installed. Please install it before running the script."
-    exit 1
-  fi
+function check_binaries() {
+  local missing_binaries=()
+  local bold=$(tput bold)
 
-  if ! command -v helm &>/dev/null; then
-    echo "helm is not installed. Please install it before running the script."
-    exit 1
-  fi
+  check_binary() {
+    if ! command -v "$1" &>/dev/null; then
+      missing_binaries+=("$1")
+    fi
+  }
 
-  if ! command -v kubectl &>/dev/null; then
-    echo "kubectl is not installed. Please install it before running the script."
+  check_binary minikube
+  check_binary helm
+  check_binary kubectl
+
+  if [[ ${#missing_binaries[@]} -gt 0 ]]; then
+    echo "The following binaries are not installed: ${bold}${missing_binaries[*]}. Please make sure you install \
+them before running the script."
     exit 1
   fi
 }
+
 
 function start_minikube()
 {
   if minikube status | grep -q Running; then
     echo "Minikube is already running..."
   else
-    # Start minikube
     echo "Starting minikube..."
     minikube start
 
-    # Wait for minikube to start
     until minikube status | grep -q Running; do
       sleep 1
     done
   fi
 }
 
-function check_namespace()
-{
-  echo "Creating namespace..."
-  kubectl create namespace logfilter-demo || {
-    echo "Error creating namespace logfilter-demo"
-    exit 1
-  }
+function check_namespace() {
+  local namespace="logfilter-demo"
+  if kubectl get namespace $namespace &>/dev/null; then
+    echo "Namespace $namespace already exists. Skipping..."
+  else
+    echo "Creating namespace $namespace..."
+    kubectl create namespace $namespace
+  fi
 }
+
 
 function install_chart(){
   echo "Installing OurApp Helm chart..."
